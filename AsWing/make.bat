3700197033ca0fe5d84b575746c7743a9b10d0c9
oliver <oliver@68b7e2f1-9726-0410-a5e9-c138f7cdcddd>
2008-02-20 22:48:22 +0000

    Fixed AsWing makefile output to be more precise about it's result

--- AsWing/make.bat
+++ AsWing/make.bat
@@ -4,6 +4,8 @@ REM written by Oliver "gencha" Salzburg 2008-02-20
 
 set flex_sdk_dir=path_to_flex_sdk
 
+if not (%1) == () set flex_sdk_dir=%1
+
 if (%flex_sdk_dir%) == (path_to_flex_sdk) goto sdk_missing
 if (%flex_sdk_dir%) == () goto sdk_missing
 if not exist %flex_sdk_dir% goto sdk_missing
@@ -14,19 +16,28 @@ goto make
 
 :manifest_missing
 echo You have to run configure.bat first.
-goto end
+goto fail
 
 :sdk_missing
 echo You have to set the path to the Flex SDK inside "%0.bat".
-goto end
+goto fail
 
 :make
-echo Making...
+echo Making AsWing...
 
 if not exist bin md bin
 set compc=%flex_sdk_dir%\bin\compc.exe
-%compc% -source-path src -output bin\AsWing.swc -namespace http://aswing.org src\manifest.xml -include-namespaces http://aswing.org 
+%compc% -source-path src -output bin\AsWing.swc -namespace http://aswing src\manifest.xml -include-namespaces http://aswing 
 
+if not exist bin\AsWing.swc goto fail
+goto success
+
+:success
+echo Done building AsWing.
+goto end
+
+:fail
+echo Build of AsWing failed!
+goto end
 
 :end
-echo Done.
